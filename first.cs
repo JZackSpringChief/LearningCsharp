c4378c1ef24e4116e13c7d864a54d797e139dc09
Zack Spring Chief <jzacksc@gmail.com>
2023-09-26 22:31:43 -0600

    testing json to use extention prettier

--- first.cs
+++ first.cs
@@ -5,7 +5,14 @@ public class Program
 {
     public static void Main(string[]args)
     {
-        Console.WriteLine("New Message");
+    Console.WriteLine("New Message");
+    if (this.Message == "New Message") 
+    {
+        Console.WriteLine("inbox full");
     }
+    else {
+Console.WriteLine("");
+    }
+     }
 }
 
