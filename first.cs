15e54248ef665156271f65b8608b5da4f00b3221
Zack Spring Chief <jzacksc@gmail.com>
2023-09-27 08:39:04 -0600

    adding notes with examples

--- first.cs
+++ first.cs
@@ -22,7 +22,12 @@ Console.WriteLine("");
 
 string name;
 name = "bob";
-
 //OR
-
 string name = "canada";
+// notes
+string hello = "denoted by double quotes and key word string";
+char grade = 'A'; //denoted by single qoutes and a single letter
+int num = 15; // stores whole numbers
+float height = 1.82f; //denoted by the letter 'f' at the end. uses less storage general use to use float
+decimal weight = 92.8m; //denoted by letter 'm' at the end. uses more storage but more acurate. 
+bool isOpen = true; // or false denoted by keywords true or false. working with states or conditions.
