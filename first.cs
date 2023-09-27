d4420b657eb4cc98ac4bd17dd28077b2cb90b117
Zack Spring Chief <jzacksc@gmail.com>
2023-09-27 08:51:44 -0600

    adding chapter 1 notes

--- first.cs
+++ first.cs
@@ -31,3 +31,8 @@ int num = 15; // stores whole numbers
 float height = 1.82f; //denoted by the letter 'f' at the end. uses less storage general use to use float
 decimal weight = 92.8m; //denoted by letter 'm' at the end. uses more storage but more acurate. 
 bool isOpen = true; // or false denoted by keywords true or false. working with states or conditions.
+
+
+//math with numbers follows real math rules BEDMAS
+// Console.WriteLine(); to generate output
+// using System; at begining and follow note at top.
