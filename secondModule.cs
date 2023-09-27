175cf7e2c725253f087f5f4d52d74ab5e0bb1072
Zack Spring Chief <jzacksc@gmail.com>
2023-09-27 09:16:00 -0600

    notes for operators

--- secondModule.cs
+++ secondModule.cs
@@ -15,3 +15,8 @@ public static void Main(string[] args)
 
 //'%' operator gets the remainder
 //++ and -- can increment and decrement respectivly also x = 10 write like this(x++) will still be 10 however, written like this (++x) will become 11 same for decrement.
+// assignment operators example int score = 10; score+=10; adds 10 in this ex. "+=" and "-="
+// cont can use assignment operators with variables ex 
+double price = 50;
+double rate = 1.2;
+price*=rate;
