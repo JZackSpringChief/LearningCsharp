7314abcaa33916d88e04c7041ce530e481767cf9
Zack Spring Chief <jzacksc@gmail.com>
2023-09-27 20:41:13 -0600

    logical operators

--- thirdModule.cs
+++ thirdModule.cs
@@ -73,3 +73,65 @@ public class Program
 		}
 	}
 }
+
+//logical operators "&&" ex. need both conditions to be true in order to be true
+using System;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		int points = 95;
+		int keys = 2;
+
+		if(points>=90 && keys >=2)
+		{
+			Console.WriteLine("Level Completed!");
+		}
+		else
+		{
+			Console.WriteLine("Try again!");
+		}
+	}
+}
+
+//logical or "||" operator only requires one condition to be true
+using System;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		int hour = 18;
+		string day = "Sunday";
+
+		if(hour>20 || day=="Sunday")
+		{
+			Console.WriteLine("Closed");
+		}
+		else
+		{
+			Console.WriteLine("Opened");
+		}
+	}
+}
+
+//the not "!" operator reverses a logical state !true is false; ex.
+using System;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		string day = "Monday";
+
+		if( !(day=="Sunday"))
+		{
+			Console.WriteLine("Working day");
+		}
+		else
+		{
+			Console.WriteLine("Non-working day");
+		}
+	}
+}
