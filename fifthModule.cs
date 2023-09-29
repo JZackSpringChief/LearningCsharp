33407be15702014977528a955561d7f46bcbb238
Zack Spring Chief <jzacksc@gmail.com>
2023-09-28 18:53:48 -0600

    notes to methods

--- fifthModule.cs
+++ fifthModule.cs
@@ -1,3 +1,28 @@
 //Methods
+using system;
+
+static void Login()
+[
+    Console.WriteLine("Login");
+]
+
+//calling a method.
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		Welcome();
+	}
+	static void Welcome() 
+	{
+		Console.WriteLine("Welcome");
+		Console.WriteLine("I am a method");
+		Console.WriteLine("End of method");
+        //this is inside the scope.
+	}
+}
+
+//main method is the entry point of the program, we call our method inside the scope.
 
 //sept28
